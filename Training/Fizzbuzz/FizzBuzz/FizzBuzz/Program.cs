5942c1d705cec7d0fbb2badc3e6bae0eed6c259a
Jothle13 <62017741+Jothle13@users.noreply.github.com>
2020-03-10 07:42:36 -0500

    Big Upload of Past Projects

---
+++ Training/Fizzbuzz/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+            FizzBuzz(number);
+            Console.ReadKey();
+        }
+        /*
+         * This program will recieve int X and print all numbers from 1-x 
+         * (if x is 3 it would say:
+         * 1
+         * 2
+         * 3)
+         * 
+         * Here's the catch: \
+         * Numbers that are divisible by 3 will be replaced with Fizz
+         * Numbers that are divisible by 5 will be replaced with Buzz
+         * Numbers that are divisible by both will be replaced with FizzBuzz
+         * 
+         */
+        static void FizzBuzz(int x)
+        {
+            for(int i = 1; i <= x; i++)
+            {
+
+                if(i%5 == 0 && i%3 == 0)
+                {
+                    for(int j = 0; j < i/5; j++)
+                    {
+                        Console.Write("FizzBuzz");
+                    }
+
+                    int fizzRemainder = (i/3) - (i/5);
+                    for(int j = 0; j < fizzRemainder; j++)
+                    {
+                        Console.Write("Fizz");
+                    }
+                    Console.WriteLine();
+                }
+                else if(i % 3 == 0)
+                {
+                    for (int j = 0; j < i / 3; j++)
+                    {
+                        Console.Write("Fizz");
+                    }
+                    Console.WriteLine();
+                }
+                else if(i % 5 == 0)
+                {
+                    for (int j = 0; j < i / 5; j++)
+                    {
+                        Console.Write("Buzz");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
---
+++ Training/GroupScenario2/GroupScenario2/GroupScenario2/GroupScenario2/Parse.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupScenario2
+{
+    public class Parse
+    {
+        string equationString;
+        string[] equationNumberParts;
+        string[] equationOperatorParts;
+
+        char[] allowableCharacters = new char[19] { 'x', 'y', '=', '^', '*', '/', '+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        char[] functionCharacters = new char[6] { '=', '^', '*', '/', '+', '-' };
+
+        /// <summary>
+        /// This is your input. Save your equation into this instance of the class.
+        /// </summary>
+        /// <param name="equation"></param>
+        public void ParseString(string equation)
+        {
+            string tempEquation = equation.ToLower().Trim();
+            if (tempEquation.Length == 0 || !CheckForInvalidChars(tempEquation))
+            {
+                equationString = null;
+            }
+            else
+            {
+                equationString = equation;
+                equationNumberParts = equationString.Split(functionCharacters);
+                
+
+                equationOperatorParts = equationString.Split(equationNumberParts, StringSplitOptions.None);
+                for (int i = 0; i < equationNumberParts.Length; i++)
+                {
+                    equationNumberParts[i] = equationNumberParts[i].Trim();
+                }
+
+                for (int i = 0; i < equationOperatorParts.Length; i++)
+                {
+                    equationOperatorParts[i] = equationOperatorParts[i].Trim();
+                }
+            }
+        }
+
+        public bool CheckForInvalidChars(string equation)
+        {
+            char[] tempEquationArray = equation.ToCharArray();
+            bool validEquation = true;
+
+            bool multiY = false;
+            bool multiEqual = false;
+
+            foreach(char c in tempEquationArray)
+            {
+                bool badEquation = true;
+
+                foreach(char allowedChar in allowableCharacters)
+                {
+                    if (c == allowedChar || c == ' ')
+                    {
+                        if (c == 'y' && !multiY)
+                        {
+                            multiY = true;
+                            badEquation = false;
+                            break;
+                        }
+                        else if(c == 'y' && multiY)
+                        {
+                            break;
+                        }
+                        else if (c == '=' && !multiEqual)
+                        {
+                            multiEqual = true;
+                            badEquation = false;
+                            break;
+                        }
+                        else if(c == '=' && multiEqual)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            //Found the character, break out.
+                            badEquation = false;
+                            break;
+                        }
+                    }
+                }
+
+                if(badEquation)
+                {
+                    validEquation = false;
+                    break;
+                }
+            }
+
+            return validEquation;
+        }
+
+
+        /// <summary>
+        /// Print one line for each iteration (i.e. for y=x*2 at 4 iterations:
+        /// 1: 2
+        /// 2: 4
+        /// 3: 6
+        /// 4: 8
+        /// </summary>
+        /// <param name="iterations"></param>
+        public void GetResults(int iterations)
+        {
+            if(equationString == null)
+            {
+                Console.WriteLine("Invalid input.");
+            }
+            else
+            {
+                for(int i = 1; i <= iterations; i++)
+                {
+                    
+                    Console.Write(i + ": " + "");
+                }
+            }
+        }
+
+
+    }
+}
